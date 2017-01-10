aed6a1431272fcbbf58c4435fe851b5d518772bb
Elizabeth Bronwyn <lisabronwyn@Elizabeths-MacBook-Pro.local>
2017-01-10 11:59:36 -0800

    set up index express css js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+let app = express();
+
+app.use(express.static(_dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('index');
+});
+
+let server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});
